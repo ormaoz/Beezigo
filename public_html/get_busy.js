36bb6c5394f659c0c4387f0a5ed84f53f98b7838
Ran Romano <ranromano@outlook.com>
2014-01-07 14:11:10 +0200

    3rd commit, trying to debug the AJAX calls.

---
+++ public_html/get_busy.js
@@ -0,0 +1,32 @@
+/**
+ * Created by ranromano on 1/7/14.
+ */
+function get_busy() {
+    $.ajax({
+        method: 'POST',
+        url : 'http://localhost:8888',
+        data : {
+            reqString : 'location_data',
+            loc_name  : 'times square',
+            lat       : '40.759101',
+            long      : '73.984406',
+            capacity  : '1000'
+        },
+
+        success: function(data) {
+
+
+            alert(data.meter);
+
+            // If we received a successful callback, parse the data, extract
+            // the 'count' field and assign its value to the counter
+            document.write(<p>data.meter</p>);
+
+
+        },
+
+        error : function() {
+            alert("Error: Something went wrong!");
+        }
+    })
+}
