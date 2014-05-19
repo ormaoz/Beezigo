83fe5f24dfb66fd03872dab15ee7f0f558e72b2e
ormaoz <ormaoz@gmail.com>
2014-05-19 10:14:12 +0300

    new logp

--- public_html/js/pushRegistrar.js
+++ public_html/js/pushRegistrar.js
@@ -80,7 +80,7 @@ var app = {
                 // this is the actual push notification. its format depends on the data model from the push server
                 // alert('message = '+ e.message +' msgcnt = '+ e.msgcnt);
                 // alert(e.text + ' (' + e.created + ')');
-                alert("In message");
+                alert(e.payload.name);
                 break;
 
             case 'error':
